9a8e5929e0e1cb8300b56f4305653184da9aec84
giladondon@gmail.com <takeIt123>
2016-04-27 18:29:19 +0300

    ChatClasses added

--- ChatClasses.py
+++ ChatClasses.py
@@ -21,6 +21,7 @@ class ChatUser(object):
         :param is_muted: is this user muted in chat
         """
         self.user_name = user_name
+        self.name_length = len(user_name)
         self.is_admin = is_admin
         self.is_muted = is_muted
 
